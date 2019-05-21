30fe688a2f06686aca306b94ab15c6f3ca4bb0fc
Khanh Doan <admin@khanh.pro>
2019-05-22 02:26:20 +0700

    xport

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-	
+import api from './helpers/api';
+import cache from './helpers/cache';
+import cart from './helpers/cart';
+import cookie from './helpers/cookie';
+import currency from './helpers/currency';
+import image from './helpers/image';
+import JsonApi from './helpers/JsonApi';
+import options from './helpers/options';
+import seo from './helpers/seo';
+
+import cartStore from './store/cart';
+
+module.exports = {
+	api, cache, cart, cookie, currency, image, JsonApi, options, seo,
+  cartStore
 }
