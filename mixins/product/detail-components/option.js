057570ba07dba81a3705622bee0ebfd9e5cbf90b
Khanh Doan <admin@khanh.pro>
2019-06-27 12:09:23 +0700

    product options and variant concept change

--- mixins/product/detail-components/option.js
+++ mixins/product/detail-components/option.js
@@ -1,180 +1,39 @@
 export default {
 
-  props: ['option', 'showAllOptions', 'optionValues', 'activeVariant'],
+  props: ['option', 'index'],
 
   data() {
 
     return {
-
-      optionType: null,
-
-      supportedColors: {
-        black: '#111111',
-        white: '#ffffff',
-        chocolate: '#381d1b',
-        grey: '#595959',
-        purple: '#4A397F',
-        ash: '#CCCCCC',
-        navy: '#0A2245',
-        blue: '#054ae8',
-        'light blue': '#8bb8e8',
-        orange: '#FF6600',
-        yellow: '#FFaa02',
-        gold: '#FFaa02',
-        'dark green': '#154734',
-        green: '#00984F',
-        pink: '#FB4788',
-        red: '#b1202d',
-
-      },
-      optionValueColorMap: {}
-
+      selectedOptionValue: null
     };
 
   },
 
   methods: {
 
-    getOptionType() {
+    /**
+    * Check option value is active
+    */
+    checkOptionValueActive(value) {
 
-      if (this.optionType) {
-        return this.optionType;
-      }
-
-      if (this.option.attributes.option_type === 'color' || this.option.attributes.name.search(/color/i) > -1) {
-
-        // Check option values in list supported color
-        for (let i = 0; i < this.optionValues.length; i++) {
-
-          let isColor = false;
-          let optionValue = this.optionValues[i];
-
-          for (let key in this.supportedColors) {
-
-            let search = new RegExp(key, 'i');
-
-            if (optionValue.attributes.value.search(search) > -1) {
-              isColor = true;
-              this.optionValueColorMap[optionValue.attributes.value] = this.supportedColors[key];
-              break;
-            }
-          }
-
-          if (!isColor) {
-            this.optionType = 'normal';
-            return this.optionType;
-          }
-
-        }
-
-        this.optionType = 'color';
-        return this.optionType;
-
-      }
-
-      this.optionType = 'normal';
-      return this.optionType;
-
-    },
-
-    checkActive(optionValueId) {
-
-      if (!this.activeVariant) {
+      if (!this.$parent.activeVariant) {
         return false;
       }
 
-      for (let i = 0; i < this.activeVariant.relationships.option_values.data.length; i++) {
-        if (optionValueId === this.activeVariant.relationships.option_values.data[i].id) {
-          return true;
-        }
-      }
+      return this.$parent.activeVariant.attributes['option' + (index+1)] === value;
 
-      return false;
-
-    },
-
-    show(optionValueId) {
-      
-      if (this.showAllOptions) {
-        return true;
-      }
-
-      return this.checkAvailable(optionValueId);
-
-    },
-
-    // Check if an option value is available
-    checkAvailable(optionValueId) {
-
-      let unavailableVariants = this.$parent.unavailableVariants;
-
-      let compareDataMaker = variant => {
-
-        let ids = [];
-
-        for (let k = 0; k < variant.relationships.option_values.data.length; k++) {
-
-          if (this.optionValueIds.indexOf(variant.relationships.option_values.data[k].id) === -1) {
-            ids.push(variant.relationships.option_values.data[k].id);
-          }
-
-        }
-
-        ids.sort();
-
-        return JSON.stringify(ids);
-
-      };
-
-      // Find in unavailable variants
-      for (let i = 0; i < unavailableVariants.length; i++) {
-
-        let found = false;
-
-        // Find in option_values
-        for (let k = 0; k < unavailableVariants[i].relationships.option_values.data.length; k++) {
-
-          if (unavailableVariants[i].relationships.option_values.data[k].id === optionValueId) {
-
-            // Found this unavailable
-            found = unavailableVariants[i];
-          }
-
-        }
-
-        if (!found) {
-          continue;
-        }
-
-        // Compare with active variant to see if this option is unavailable
-        if (compareDataMaker(this.activeVariant) === compareDataMaker(found)) {
-
-          // Match - so this option is unavailable
-          return false;
-
-        }
-
-      }
-
-      return true;
-    },
-
-    changeOptionValue(id) {
-      this.$parent.$emit('change-option-value', {
-        activeId: id, // New active id
-        currentId: this.activeOptionValueId // Current active ID
-      });
     },
 
     getOptionGuide() {
 
       return new Promise((resolve, reject) => {
 
-        if (!this.option.attributes.option_guide_id) {
+        if (!this.option.option_guide_id) {
           return resolve(null)
         }
 
-        this.$hiwebBase.api.get('option_guides/' + this.option.attributes.option_guide_id).then(response => {
+        this.$hiwebBase.api.get('option_guides/' + this.option.option_guide_id).then(response => {
           return resolve(response.data.data);
         }).catch(error => {
           return reject(error);
@@ -186,57 +45,25 @@ export default {
 
   },
 
+  watch: {
+
+    selectedOptionValue: function(value) {
+      this.$parent.selectedOptions['option' + (this.index+1)] = value;
+    }
+
+  },
+
   computed: {
 
-    optionValueIds: function() {
+    activeVariant: {
 
-      let ids = [];
+      get: function() {
+        return this.$parent.activeVariant;
+      },
 
-      for (let i = 0; i < this.optionValues.length; i++) {
-        ids.push(this.optionValues[i].id);
+      set: function(value) {
+        this.$parent.activeVariant = value;
       }
-
-      return ids;
-
-    },
-
-    activeOptionValueId: function() {
-
-      if (!this.activeVariant) {
-        return null;
-      }
-
-      for (let i = 0; i < this.activeVariant.relationships.option_values.data.length; i++) {
-
-        for (let k = 0; k < this.optionValues.length; k++) {
-
-          if (this.optionValues[k].id === this.activeVariant.relationships.option_values.data[i].id) {
-            return this.optionValues[k].id;
-          }
-
-        }
-
-      }
-
-      return null;
-
-    },
-
-    activeOptionValue: function() {
-
-      let id = this.activeOptionValueId;
-
-      if (!id) {
-        return null;
-      }
-
-      for (let i = 0; i < this.optionValues.length; i++) {
-        if (id === this.optionValues[i].id) {
-          return this.optionValues[i];
-        }
-      }
-
-      return null;
 
     }
 
--- mixins/product/detail.js
+++ mixins/product/detail.js
@@ -8,8 +8,13 @@ export default {
       
       productJsonApi: null,
 
-      activeVariantId: null,
       activeVariant: null,
+
+      selectedOptions: {
+        option1: null,
+        option2: null,
+        option3: null
+      },
       
       qty: 1,
 
@@ -27,99 +32,10 @@ export default {
     this.loadData();
 
     // Listen to message event
-    window.addEventListener('message', data => {
+    window.addEventListener('options-updated', () => {
       this.$forceUpdate();
     });
 
-    // Option value change
-    this.$on('change-option-value', data => {
-
-      let variants = this.productJsonApi.findRelationshipResources(this.productJsonApi.document.data, 'variants');
-
-      let newOptionValueData;
-      if (this.activeVariant) {
-
-        newOptionValueData = JSON.parse(JSON.stringify(this.activeVariant.relationships.option_values.data));
-
-        for (let i = 0; i < newOptionValueData.length; i++) {
-
-          if (newOptionValueData[i].id === data.currentId) {
-            newOptionValueData[i].id = data.activeId;
-            break;
-          }
-
-        }
-
-      }
-        
-      // Compare data maker
-      let compareDataMaker = function(optionValueData) {
-        
-        let compareData = [];
-
-        for (let i = 0; i < optionValueData.length; i++) {
-          compareData.push(optionValueData[i].id);
-        }
-
-        compareData.sort();
-
-        return JSON.stringify(compareData);
-
-      }
-
-      // If active variant is set
-      if (this.activeVariant) {
-      
-        // Find new active variant
-        for (let i = 0; i < variants.length; i++) {
-
-          let compareOptionValueData = JSON.parse(JSON.stringify(variants[i].relationships.option_values.data));
-          compareOptionValueData.sort();
-
-          if (compareDataMaker(newOptionValueData) === compareDataMaker(compareOptionValueData)) {
-
-            this.activeVariantId = variants[i].id;
-
-            // If found but unavailable
-            if (this.unavailableVariantIds.indexOf(this.activeVariantId) > -1) {
-              this.activeVariantId = null;
-              this.activeVariant = null;
-            } else {
-
-              // Set active variant
-              this.setActiveVariant();
-              return;
-
-            }
-
-          }
-
-        }
-
-      }
-
-      // If active variant is not set
-      if (!this.activeVariant) { 
-
-        for (let i = 0; i < variants.length; i++) {
-
-          // Find a variant available with selected option
-          for (let k = 0; k < variants[i].relationships.option_values.data.length; k++) {
-
-            // Found and available
-            if (variants[i].relationships.option_values.data[k].id === data.activeId && this.unavailableVariantIds.indexOf(variants[i].id) === -1) {
-              this.activeVariantId = variants[i].id;
-              this.setActiveVariant();
-              return;
-            }
-
-          }
-
-        }
-
-      }
-
-    });
 
   },
 
@@ -182,39 +98,7 @@ export default {
 
     setActiveVariant: function() {
 
-      // If all variants are not available
-      if (this.unavailableVariants.length === this.productJsonApi.document.data.relationships.variants.data.length) {
-        return false;
-      }
-
-      // Lowest price variant will be activated by default
-      if (!this.activeVariantId) {
-
-        let lowestPrice = 0;
-          
-        for (let i = 0; i < this.productJsonApi.document.data.relationships.variants.data.length; i++) {
-
-          let variant = this.productJsonApi.findIncludedResource('variants', this.productJsonApi.document.data.relationships.variants.data[i].id);
-
-          if (this.unavailableVariantIds.indexOf(variant.id) === -1 && (!lowestPrice || variant.attributes.price < lowestPrice)) {
-            this.activeVariantId = variant.id;
-            lowestPrice = variant.attributes.price;
-          }
-
-        }
-
-
-      }
-
-      let variants = this.productJsonApi.findRelationshipResources(this.productJsonApi.document.data, 'variants');
-
-      for (let i = 0; i < variants.length; i++) {
-
-        if (variants[i].id === this.activeVariantId) {
-          return this.activeVariant = variants[i];
-        }
-
-      }
+      
     },
 
     changeQty(number) {
@@ -223,20 +107,19 @@ export default {
 
     async addToCart() {
 
-      // Check variant available
-      if (this.unavailableVariantIds.indexOf(this.activeVariantId) > -1) {
-        return alert('The option you selected (' + this.activeVariant.attributes.title + ') is not available.');
+      if (!this.allOptionsSelected) {
+        return this.optionsReminder();
       }
 
-      if (!this.activeVariantId) {
-        return alert('Please select your option first');
+      if (!this.activeVariant || this.activeVariant.attributes.status !== 'available') {
+        return this.outOfStock();
       }
 
       this.addingToCart = true;
 
       let error = null;
 
-      let cartItem = await this.$hiwebBase.cart.add(this.activeVariantId, this.qty).catch(error => {
+      let cartItem = await this.$hiwebBase.cart.add(this.activeVariant.id, this.qty).catch(error => {
         error = true;
       });
 
@@ -250,6 +133,14 @@ export default {
       // Dispatch event
       window.dispatchEvent(new Event('add-to-cart'));
 
+    },
+
+    optionsReminder() {
+      alert('Please select all options');
+    },
+
+    outOfStock() {
+      alert('Sorry, this item is out of stock or not available at the moment');
     }
 
   },
@@ -264,38 +155,36 @@ export default {
 
     slug: function() {
       this.loadData();
+    },
+
+    selectedOptionsWatcher: function() {
+
+      // Find active variant
+      for (let i = 0; i < this.variants.length; i++) {
+
+        if (this.variants[i].attributes.option1 === this.selectedOptions.option1
+          && this.variants[i].attributes.option2 === this.selectedOptions.option2
+          && this.variants[i].attributes.option3 === this.selectedOptions.option3
+          ) {
+          this.activeVariant = this.variants[i];
+          return;
+        }
+
+      }
+
+      this.activeVariant = null;
+
     }
 
   },
 
   computed: {
 
-    unavailableVariantIds() {
-
-      let ids = [];
-
-      for (let i = 0; i < this.unavailableVariants.length; i++) {
-        ids.push(this.unavailableVariants[i].id);
-      }
-
-      return ids;
-
-    },
-
-    unavailableVariants() {
-
-      let unavailableVariants = [];
+    variants: function() {
+    
       let variants = this.productJsonApi.findRelationshipResources(this.productJsonApi.document.data, 'variants');
-
-      for (let i = 0; i < variants.length; i++) {
-
-        if (variants[i].attributes.status !== 'available') {
-          unavailableVariants.push(variants[i]);
-        }
-
-      }
-
-      return unavailableVariants;
+        
+      return variants ? variants : [];
 
     },
 
@@ -328,6 +217,32 @@ export default {
 
       return tagIds;
 
+    },
+
+    allOptionsSelected() {
+
+      switch (this.productJsonApi.document.data.attributes.options.length) {
+
+        case 1:
+        return this.selectedOptions.option1 ? true : false;
+        break;
+
+        case 2:
+        return (this.selectedOptions.option1 && this.selectedOptions.option2) ? true : false;
+        break;
+
+        case 3:
+        return (this.selectedOptions.option1 && this.selectedOptions.option2 && this.selectedOptions.option3) ? true : false;
+        break
+
+      }
+
+      return true;
+
+    },
+
+    selectedOptionsWatcher: function() {
+      return JSON.stringify(this.selectedOptions);
     }
 
   }
